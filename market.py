e37035d6e39fe42ec257eb6cde9626dd898edf2c
Douglas Smith <douglassmith@Douglass-MacBook-Pro.local>
2017-12-03 15:35:37 -0500

    newFeatures

--- market.py
+++ market.py
@@ -4,15 +4,11 @@ import math
 import datetime
 from copy import copy, deepcopy
 
-def dateparse (time_in_secs):    
-    return datetime.datetime.fromtimestamp(float(time_in_secs))
-
 
 class BitcoinMarket():
-	def __init__(self, fileName="coinbaseUSD_1-min_data_2014-12-01_to_2017-10-20.csv"):
-		self.marketData = pd.read_csv(fileName, parse_dates=True,date_parser=dateparse, index_col='Timestamp') 
+	def __init__(self, fileName="15min_fulldata.csv"):
+		self.marketData = pd.read_csv(fileName, parse_dates=True) 
 		self.marketData.reset_index(level=0, inplace=True)
-		self.marketData.columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume_btc', 'volume_currency', 'weighted_price']
 		self.curTimestamp = 1
 		self.numTimestamps = len(self.marketData)
 
@@ -44,11 +40,11 @@ class BitcoinMarket():
 	def setTimestamp(self, timestamp):
 		self.curTimestamp = timestamp
 
-	def setSampledTimestamp(self, upperbound):
+	def setSampledTimestamp(self, lowerbound, upperbound):
 		"""
 			upperbound is a decimal (i.e. 0.8 to sample from the first 80% of timestamps)
 		"""
-		self.curTimestamp = random.randint(1, math.floor(self.numTimestamps * upperbound))
+		self.curTimestamp = random.randint(math.ceil(self.numTimestamps * lowerbound), math.floor(self.numTimestamps * upperbound))
 		return self.curTimestamp
 
 	def incrementTime(self):
