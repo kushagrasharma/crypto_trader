7d88db426b6f206df8b511e169e15fd104cce734
Kushagra Sharma <ksharm01@gmail.com>
2017-12-03 15:27:57 -0500

    preprocessing

--- extractors.py
+++ extractors.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+0
+import time
 
 class FeatureExtractor():
 	def getFeatures(state, action):
@@ -66,24 +69,23 @@ class Try1(FeatureExtractor):
 			"10": market["weighted_price"] - pastDay["weighted_price"]
 		}
 
-
+"""
 def willr(state, action):
 	"""
-		Determines where today's closing price fell within the range on past 10-day's transaction. 
+		#Determines where today's closing price fell within the range on past 10-day's transaction. 
 	"""
 	market = state["market"]
 	cur = market.getCurrentMarketInfo()
 	past = [market.getPastMarketInfo(i) for i in range(10)]
 	high = max([x["high"] for x in past])
 	low = min([x["low"] for x in past])
-
 	if high - low == 0:
 		return 0
 	return (high - cur["close"]) / (high - low) * 100
 
 def rocr(state, action, n):
 	"""
-		Compute rate of change relative to previous trading intervals
+		#Compute rate of change relative to previous trading intervals
 	"""
 	market = state["market"].getCurrentMarketInfo()
 	pastN = state["market"].getPastMarketInfo(n)
@@ -91,24 +93,28 @@ def rocr(state, action, n):
 
 def momentum(state, action, n):
 	"""
-		Measures the change in price
+		#Measures the change in price
 	"""
 	market = state["market"]
 	cur = market.getCurrentMarketInfo()
 	past = market.getPastMarketInfo(n)
 	return cur["weighted_price"] - past["weighted_price"]
 
-def RSI(state, window_length=14):
+def RSI_self(state, window_length=14, libr=False):
 	pasts = []
 	for i in range(window_length, 0, -1):
 		pasts.append(state["market"].getPastMarketInfo(1440*i).close)
-	pasts = pd.Series(pasts)
-	delta = pasts.diff() 
-	delta = delta[1:]
-	up, down = delta.copy(), delta.copy()
-	up[up < 0] = 0
-	down[down > 0] = 0
-	roll_up1 = pd.stats.moments.ewma(up, window_length)
-	roll_down1 = pd.stats.moments.ewma(down.abs(), window_length)
-	RS1 = roll_up1 / roll_down1
-	return (100.0 - (100.0 / (1.0 + RS1))).iloc[-1]
+	pasts.append(state['market'].getCurrentMarketInfo().close)
+	return RSI(pd.DataFrame(pasts, index=np.arange(len(pasts)), columns=['close']), timeperiod=window_length).iloc[-1]
+"""
+def aggregate(data):
+    data.sort_values('timestamp')
+    agged = {'timestamp' : data.iloc[0].timestamp, 
+             'high' : np.max(data.high),
+             'low' : np.min(data.low),
+             'close' : data.iloc[-1].close,
+             'open' : data.iloc[0].open,
+             'volume_btc' : np.sum(data.volume_btc),
+             'volume_currency' : np.sum(data.volume_currency)}
+    agged['weighted_price'] = np.mean([agged['high'], agged['low'], agged['close'], agged['open']])
+    return pd.DataFrame(agged, index=[0], columns=['timestamp', 'high', 'low', 'close', 'open', 'volume_btc', 'volume_currency'])
